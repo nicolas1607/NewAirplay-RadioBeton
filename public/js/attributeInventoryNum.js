2291bfb46305d83a1ac1c8bc765397c9ee7c6dbb
Nicolas Mormiche <nicolas160796@gmail.com>
2022-02-10 10:00:30 +0100

    [FIX] Générer un num_inventory

--- public/js/attributeInventoryNum.js
+++ public/js/attributeInventoryNum.js
@@ -1,10 +1,8 @@
-
 // Gère le numéro d'inventaire à 0 ou non
 const numInventory = document.querySelector('#valueNumInventory input').value;
 document.querySelector('#valueNumInventory input').value = 0;
 
 const leaveDate = document.querySelector('#disc_leaveDate');
-
 leaveDate.addEventListener('change', () => {
     document.querySelector('#valueNumInventory input').value = numInventory;
 });
@@ -12,10 +10,14 @@ leaveDate.addEventListener('change', () => {
 // Gère le boutton 'Générer numéro d'inventaire'
 const generateNumInventory = document.querySelector('#generateNumInventory');
 const valueNumInventory = document.querySelector('#valueNumInventory');
-
 if (generateNumInventory) {
     generateNumInventory.addEventListener('click', () => {
         valueNumInventory.style.display = 'block';
         generateNumInventory.remove();
     });
+}
+
+if (numInventory != "0") {
+    document.querySelector('#valueNumInventory input').value = numInventory;
+    generateNumInventory.click();
 }
