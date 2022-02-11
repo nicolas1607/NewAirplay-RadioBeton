a0f437e7d2122b8ca0c56fe235da518a53ef099f
Jérôme Salmon <hyeronimus.salmo.2017@gmail.com>
2022-02-11 14:44:25 +0100

    [JS] création d'un fichier dédié aux chexboxes de l'ajout de disque

--- public/js/addTrackstoList.js
+++ public/js/addTrackstoList.js
@@ -1,16 +1,17 @@
-const divCheck = document.querySelector('.checkBox');
-const divChildCheck = divCheck.childNodes;
 
-divChildCheck[1].classList.add('form-check');
-divChildCheck[1].classList.add('form-switch');
-divChildCheck[3].classList.add('form-check');
-divChildCheck[3].classList.add('form-switch');
-divChildCheck[5].classList.add('form-check');
-divChildCheck[5].classList.add('form-switch');
+// const divCheck = document.querySelector('.checkBox');
+// const divChildCheck = divCheck.childNodes;
 
-const checkBox = document.querySelector('.form-check-input');
-console.log(checkBox);
-checkBox.setAttribute('role', 'switch');
+// divChildCheck[1].classList.add('form-check');
+// divChildCheck[1].classList.add('form-switch');
+// divChildCheck[3].classList.add('form-check');
+// divChildCheck[3].classList.add('form-switch');
+// divChildCheck[5].classList.add('form-check');
+// divChildCheck[5].classList.add('form-switch');
+
+// const checkBox = document.querySelector('.form-check-input');
+// console.log(checkBox);
+// checkBox.setAttribute('role', 'switch');
 
 const numero = document.querySelector('#numero');
 const addNumero = document.querySelector('#add-numero');
